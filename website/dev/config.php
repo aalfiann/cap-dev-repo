82074ba367fcc485857172e45d2573f079bc72dd
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-02-19 20:34:37 +0700

    - Change folder for development test

---
+++ website/dev/config.php
@@ -0,0 +1,7 @@
+<?php 
+            //Configurations
+            $config['title'] = 'CAP Express'; //Your title website
+            $config['email'] = 'webmaster@cap-express.co.id'; //Your default email
+            $config['basepath'] = 'http://cap-express.co.id/dev'; //Your folder website
+            $config['api'] = 'http://server.cap-express.co.id/api'; //Your folder rest api
+            $config['apikey'] = ''; //Your api key, you can leave this blank and fill this later
