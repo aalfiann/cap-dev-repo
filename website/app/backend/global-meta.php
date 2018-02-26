2ef57dac695ef2f6599fcb72d6f9445458d80ab6
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-02-26 18:06:02 +0700

    - Add panel for settings theme

--- website/app/backend/global-meta.php
+++ website/app/backend/global-meta.php
@@ -10,8 +10,7 @@
     <link href="../assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom CSS -->
     <link href="css/style.css" rel="stylesheet">
-    <!-- You can change the theme colors from here -->
-    <link href="css/colors/blue.css" id="theme" rel="stylesheet">
+    
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
