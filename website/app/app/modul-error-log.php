3b5a4f5a973e804a0fa29900527a1d0a4d3c9f31
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-03-03 01:58:27 +0700

    - Add module error log menu

--- website/app/app/modul-error-log.php
+++ website/app/app/modul-error-log.php
@@ -45,29 +45,51 @@ $datalogin = Core::checkSessions();?>
             <!-- ============================================================== -->
             <div class="container-fluid">
                 <!-- ============================================================== -->
-                <!-- Bread crumb and right sidebar toggle -->
-                <!-- ============================================================== -->
-                <div class="row page-titles">
-                    <div class="col-md-5 col-8 align-self-center">
-                        <h3 class="text-themecolor m-b-0 m-t-0"><?php echo Core::lang('error_log')?></h3>
-                        <p class="text-muted"><?php echo Core::lang('develop_process_info')?></p>
-                        
-                    </div>
-                </div>
-                <!-- ============================================================== -->
-                <!-- End Bread crumb and right sidebar toggle -->
-                <!-- ============================================================== -->
-                <!-- ============================================================== -->
                 <!-- Start Page Content -->
                 <!-- ============================================================== -->
                 <div class="row">
                     <div class="col-12">
+                        <?php
+                            if (isset($_POST['clearlog'])){
+                                $data = json_decode(Core::execGetRequest(Core::getInstance()->api.'/logs/data/clear/'.$datalogin['username'].'/'.$datalogin['token']));
+                                if (!empty($data)){
+                                    if ($data->{'status'} == "success"){
+                                        echo '<div class="col-lg-12">';
+                                        echo Core::getMessage('success',Core::lang('core_clear_log_success'));
+                                        echo '</div>';
+                                    } else {
+                                        echo '<div class="col-lg-12">';
+                                        echo Core::getMessage('danger',Core::lang('core_clear_log_failed'),$data->{'message'});
+                                        echo '</div>';
+                                    }
+                                } else {
+                                    echo '<div class="col-lg-12">';
+                                    echo Core::getMessage('danger',Core::lang('core_clear_log_failed'),Core::lang('core_not_connected'));
+                                    echo '</div>';
+                                }
+                            } 
+                        ?>
                         <div class="card">
                             <div class="card-body">
-                                <h4 class="card-title" id="1">1. Title will be here</h4>
-                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi.</p>
-                                <p> enean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar,</p>
-                                <p> enean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar,</p>
+                                <h4 class="card-title"><?php echo Core::lang('error_log_title')?></h4><hr>
+                                <h6 class="card-subtitle"><?php echo Core::lang('error_log_description')?></h6>
+                                <form method="post" action="<?php $_SERVER['PHP_SELF']?>">
+                                    <div class="form-group">
+                                        <?php 
+                                            if (!empty(Core::getInstance()->api)){
+                                                $urlarray = explode("/",Core::getInstance()->api);
+                                                array_pop($urlarray);
+                                                $urlhost = implode('/', $urlarray);
+                                                $url = $urlhost.'/logs/app.log';
+                                            }
+                                            echo '<textarea id="textarea_1" name="content" class="form-control" rows="20" >'.(!empty($url)?Core::execGetRequest($url):'').'</textarea>';
+                                        ?>
+                                    </div>
+                                    <hr>
+                                    <div class="form-group text-center">
+                                        <button name="clearlog" type="submit" class="btn btn-danger"><?php echo Core::lang('clear_log')?></button>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
@@ -90,6 +112,20 @@ $datalogin = Core::checkSessions();?>
     <!-- End Wrapper -->
     <!-- ============================================================== -->
     <?php include_once 'global-js.php';?>
+    <!-- Edit_area -->
+	<script language="javascript" type="text/javascript" src="../assets/plugins/edit_area/edit_area_full.js"></script>
+   <script language="javascript" type="text/javascript">
+    editAreaLoader.init({
+	    id : "textarea_1",
+	    start_highlight: false,	
+	    allow_resize: "y",
+    	allow_toggle: true,
+        word_wrap: false,
+        show_line_colors: true,
+	    language: "<?php echo Core::getInstance()->setlang?>",
+    	syntax: "html"
+    });
+ 	</script>
 </body>
 
 </html>
