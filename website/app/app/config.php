afb3a07bf78d26b70b960336233e6900082527d6
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-03-08 00:40:13 +0700

    - Set default config for CAP

--- website/app/app/config.php
+++ website/app/app/config.php
@@ -1,11 +1,11 @@
 <?php 
             //Configurations
-            $config['title'] = 'reSlim'; //Your title website
-            $config['keyword'] = 'Backend, Rest, API, UI'; //Your keyword website
-            $config['description'] = 'Make your project reSlim with use restful api. reSlim is based on Slim3'; //Your description website
-            $config['email'] = 'aalfiann@gmail.com'; //Your default email
-            $config['basepath'] = 'http://localhost:1337/cap-dev-repo-new/website/app/app'; //Your folder website
-            $config['homepath'] = 'http://localhost:1337/cap-dev-repo-new/website/app/app'; //Your folder frontend website
+            $config['title'] = 'CAP Express'; //Your title website
+            $config['keyword'] = 'CAP Express, Logistics, Blog, Transport, Corporate, Cargo, Truck, Jasa Pengiriman, Pengiriman Barang Murah, Ekspedisi'; //Your keyword website
+            $config['description'] = 'CAP Express adalah jasa pengiriman barang murah yang handal dan terpercaya.'; //Your description website
+            $config['email'] = 'cs@cap-express.co.id'; //Your default email
+            $config['basepath'] = 'http://localhost:1337/cap-dev-repo/website/app/app'; //Your folder website
+            $config['homepath'] = 'http://localhost:1337/cap-dev-repo/website/app/app'; //Your folder frontend website
             $config['api'] = 'http://server.cap-express.co.id/api'; //Your folder rest api
             $config['apikey'] = ''; //Your api key, you can leave this blank and fill this later
             $config['disqus'] = ''; //Your disqus username, you can leave this blank and fill this later
