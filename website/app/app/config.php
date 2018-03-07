22b6c08c448bb4b031922d483c83f65a67f553d5
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-03-07 17:40:43 +0700

    - Change config

--- website/app/app/config.php
+++ website/app/app/config.php
@@ -4,8 +4,8 @@
             $config['keyword'] = 'Backend, Rest, API, UI'; //Your keyword website
             $config['description'] = 'Make your project reSlim with use restful api. reSlim is based on Slim3'; //Your description website
             $config['email'] = 'aalfiann@gmail.com'; //Your default email
-            $config['basepath'] = 'http://localhost:1337/cap-dev-repo/website/app/app'; //Your folder website
-            $config['homepath'] = 'http://localhost:1337/cap-dev-repo/website/app/app'; //Your folder frontend website
+            $config['basepath'] = 'http://localhost:1337/cap-dev-repo-new/website/app/app'; //Your folder website
+            $config['homepath'] = 'http://localhost:1337/cap-dev-repo-new/website/app/app'; //Your folder frontend website
             $config['api'] = 'http://server.cap-express.co.id/api'; //Your folder rest api
             $config['apikey'] = ''; //Your api key, you can leave this blank and fill this later
             $config['disqus'] = ''; //Your disqus username, you can leave this blank and fill this later
