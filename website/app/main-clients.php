a0fda025b4c60ff1de85a20d5053ab9e2b38e2e4
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-03-08 16:34:16 +0700

    - Update data client CAP

--- website/app/main-clients.php
+++ website/app/main-clients.php
@@ -26,27 +26,27 @@
 								
 								<!-- === client item === -->
 								<div class="home-clients-carousel-item">
-									<a href="#"><img src="media/clients/client1.png" alt="" /></a>
+									<a href="#" title="Adira Finance"><img src="media/clients/logo-adira.png" alt="Adira Finance" /></a>
 								</div>
 								<!-- === client item === -->
 								<div class="home-clients-carousel-item ">
-									<a href="#"><img src="media/clients/client2.png" alt="" /></a>
+									<a href="#" title="Aero Express"><img src="media/clients/logo-aero-express.png" alt="Aero Express" /></a>
 								</div>
 								<!-- === client item === -->
 								<div class="home-clients-carousel-item">
-									<a href="#"><img src="media/clients/client3.png" alt="" /></a>
+									<a href="#" title="Aryaduta Hotel"><img src="media/clients/logo-aryaduta.png" alt="Aryaduta Hotel" /></a>
 								</div>
 								<!-- === client item === -->
 								<div class="home-clients-carousel-item">
-									<a href="#"><img src="media/clients/client4.png" alt="" /></a>
+									<a href="#" title="Astraworld"><img src="media/clients/logo-astraworld.png" alt="Astraworld" /></a>
 								</div>
 								<!-- === client item === -->
 								<div class="home-clients-carousel-item">
-									<a href="#"><img src="media/clients/client5.png" alt="" /></a>
+									<a href="#" title="Bank BNI"><img src="media/clients/logo-bank-bni.png" alt="Bank BNI" /></a>
 								</div>
 								<!-- === client item === -->
 								<div class="home-clients-carousel-item">
-									<a href="#"><img src="media/clients/client6.png" alt="" /></a>
+									<a href="#" title="Bank Mandiri"><img src="media/clients/logo-bank-mandiri.png" alt="Bank Mandiri" /></a>
 								</div>
 								<div class="home-clients-carousel-hline"></div>
 								<div class="home-clients-carousel-vline1"></div>
@@ -58,69 +58,38 @@
 								
 								<!-- === client item === -->
 								<div class="home-clients-carousel-item">
-									<a href="#"><img src="media/clients/client1.png" alt="" /></a>
+									<a href="#" title="Bank Permata"><img src="media/clients/logo-bank-permata.png" alt="Bank Permata" /></a>
 								</div>
 								<!-- === client item === -->
 								<div class="home-clients-carousel-item ">
-									<a href="#"><img src="media/clients/client2.png" alt="" /></a>
+									<a href="#" title="Majalah Femina"><img src="media/clients/logo-femina.png" alt="Majalah Femina" /></a>
 								</div>
 								<!-- === client item === -->
 								<div class="home-clients-carousel-item">
-									<a href="#"><img src="media/clients/client3.png" alt="" /></a>
+									<a href="#" title="Majalah Hidayatullah"><img src="media/clients/logo-hidayatullah.png" alt="Majalah Hidayatullah" /></a>
 								</div>
 								<!-- === client item === -->
 								<div class="home-clients-carousel-item">
-									<a href="#"><img src="media/clients/client4.png" alt="" /></a>
+									<a href="#" title="Indosat IM3"><img src="media/clients/logo-im3.png" alt="Indosat IM3" /></a>
 								</div>
 								<!-- === client item === -->
 								<div class="home-clients-carousel-item">
-									<a href="#"><img src="media/clients/client5.png" alt="" /></a>
+									<a href="#" title="Kawasaki Motor Indonesia"><img src="media/clients/logo-kawasaki.png" alt="Kawasaki Motor Indonesia" /></a>
 								</div>
 								<!-- === client item === -->
 								<div class="home-clients-carousel-item">
-									<a href="#"><img src="media/clients/client6.png" alt="" /></a>
+									<a href="#" title="PALYJA"><img src="media/clients/logo-palyja.png" alt="PALYJA" /></a>
 								</div>
 								<div class="home-clients-carousel-hline"></div>
 								<div class="home-clients-carousel-vline1"></div>
 								<div class="home-clients-carousel-vline2"></div>
 							</div>
-							
-							<!-- === OWL CAROUSEL ITEM === -->
-							<div class="home-clients-carousel-block">
-								
-								<!-- === client item === -->
-								<div class="home-clients-carousel-item">
-									<a href="#"><img src="media/clients/client1.png" alt="" /></a>
-								</div>
-								<!-- === client item === -->
-								<div class="home-clients-carousel-item ">
-									<a href="#"><img src="media/clients/client2.png" alt="" /></a>
-								</div>
-								<!-- === client item === -->
-								<div class="home-clients-carousel-item">
-									<a href="#"><img src="media/clients/client3.png" alt="" /></a>
-								</div>
-								<!-- === client item === -->
-								<div class="home-clients-carousel-item">
-									<a href="#"><img src="media/clients/client4.png" alt="" /></a>
-								</div>
-								<!-- === client item === -->
-								<div class="home-clients-carousel-item">
-									<a href="#"><img src="media/clients/client5.png" alt="" /></a>
-								</div>
-								<!-- === client item === -->
-								<div class="home-clients-carousel-item">
-									<a href="#"><img src="media/clients/client6.png" alt="" /></a>
-								</div>
-								<div class="home-clients-carousel-hline"></div>
-								<div class="home-clients-carousel-vline1"></div>
-								<div class="home-clients-carousel-vline2"></div>
-							</div>
+
 						</div>
 						
 						<!-- === OWL CAROUSEL BUTTONS === -->
 						<div class="home-clients-buttons">
-							<a id="prev-clients" href="#"><div class="my-btn my-btn-primary">
+							<a id="prev-clients" href="#"><div class="my-btn my-btn-grey">
 								<div class="my-btn-bg-top"></div>
 								<div class="my-btn-bg-bottom"></div>
 								<div class="my-btn-text">
