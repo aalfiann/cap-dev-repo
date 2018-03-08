26395994941dc246d6ecdbe881bac851bff627d1
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-03-08 14:49:30 +0700

    - Add new main tracking block page

---
+++ website/app/main-tracking.php
@@ -0,0 +1,39 @@
+<!-- ===================================
+		SUBSCRIBE SECTION
+	======================================== -->
+	<div class="def-section home-subscribe">
+		<div class="container">
+			
+			<div class="row">
+				
+				<!-- === SUBSCRIBE TEXT === -->
+				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 home-subscribe-text animated wow fadeInUp" data-wow-duration=".5s" data-wow-offset="100">
+				<span class="home-subscribe-icon"><i class="flaticon-clock96"></i></span>
+					Lacak status kiriman barang Anda disini
+				</div>
+				
+				<!-- === SUBSCRIBE FORM === -->
+				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 home-subscribe-form animated wow fadeInUp" data-wow-duration=".5s" data-wow-delay=".3s" data-wow-offset="100">
+					<form>
+						<div class="home-subscribe-form-input">
+							<input type="text" name="resi" placeholder="No resi Anda..." />
+						</div>
+						<div class="home-subscribe-form-button">
+							<button><span class="my-btn my-btn-primary">
+								<span class="my-btn-bg-top"></span>
+								<span class="my-btn-bg-bottom"></span>
+								<span class="my-btn-text">
+									Cek Resi
+								</span>
+							</span></button>
+						</div>
+					</form>
+				</div>
+
+				
+			</div>
+		</div>
+	</div>
+	<!-- ===================================
+		END SUBSCRIBE SECTION
+	======================================== -->
