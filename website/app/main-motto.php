3cd587f7863a185e350eb232e9963f1b87c42ade
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-03-08 14:47:47 +0700

    - Add new main motto

---
+++ website/app/main-motto.php
@@ -0,0 +1,20 @@
+<!-- ===================================
+		SUBSCRIBE SECTION
+	======================================== -->
+	<div class="def-section home-subscribe">
+		<div class="container">
+			
+			<div class="row">
+				
+				<!-- === SUBSCRIBE TEXT === -->
+				<div class="col-lg-12 home-subscribe-text animated wow fadeInUp text-center" data-wow-duration=".5s" data-wow-offset="100">
+				<span class="home-subscribe-icon"><i class="flaticon-first33"></i></span>
+					Memberikan Tingkat Kepuasan dan Kepercayaan yang tinggi akan Kecepatan, Ketepatan Waktu dan Keamanan Pengiriman kepada Pelanggan
+				</div>
+				
+			</div>
+		</div>
+	</div>
+	<!-- ===================================
+		END SUBSCRIBE SECTION
+	======================================== -->
