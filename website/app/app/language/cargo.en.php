afcc088c698a4aea27237a1d9c240eb106d0ead9
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-04-02 01:46:33 +0700

    - Add new cargo language

---
+++ website/app/app/language/cargo.en.php
@@ -0,0 +1,15 @@
+<?php
+require_once realpath(__DIR__ . '/..').'/config.php';
+$cargolang = [
+    //menu
+    'tariff' => 'Tariff',
+    'handling' => 'Handling',
+    'district' => 'District',
+    'kgp' => 'KGP',
+    'kgs' => 'KGS',
+    'minkg' => 'Min_Kg',
+    'hkgp' => 'H_KGP',
+    'hkgs' => 'H_KGS',
+    'hminkg' => 'H_Min_Kg',
+    'estimation' => 'Estimation'
+];
---
+++ website/app/app/language/cargo.id.php
@@ -0,0 +1,15 @@
+<?php
+require_once realpath(__DIR__ . '/..').'/config.php';
+$cargolang = [
+    //menu
+    'tariff' => 'Tarif',
+    'handling' => 'Handling',
+    'district' => 'Kabupaten',
+    'kgp' => 'KGP',
+    'kgs' => 'KGS',
+    'minkg' => 'Min_Kg',
+    'hkgp' => 'H_KGP',
+    'hkgs' => 'H_KGS',
+    'hminkg' => 'H_Min_Kg',
+    'estimation' => 'Estimasi'
+];
