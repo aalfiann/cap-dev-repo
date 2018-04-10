2d3e0ffefc4315c9efbf49576819a6abddfe1975
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-04-10 14:49:00 +0700

    - Add new class insurance

---
+++ src/classes/system/cargo/Insurance.php
@@ -0,0 +1,399 @@
+<?php
+/**
+ * This class is a part of Cargo project
+ * @author M ABD AZIZ ALFIAN <github.com/aalfiann>
+ *
+ * Don't remove this class unless You know what to do
+ *
+ */
+namespace classes\system\cargo;
+use \classes\Auth as Auth;
+use \classes\Validation as Validation;
+use \classes\CustomHandlers as CustomHandlers;
+use PDO;
+	/**
+     * A class for insurance management
+     *
+     * @package    Insurance Cargo
+     * @author     M ABD AZIZ ALFIAN <github.com/aalfiann>
+     * @copyright  Copyright (c) 2018 M ABD AZIZ ALFIAN
+     * @license    https://github.com/aalfiann/reSlim-b2b/blob/master/license.md  MIT License
+     */
+	class Insurance {
+        // model data mode
+		var $username,$insuranceid,$insurance,$premium,$minpremium;
+		
+		// for pagination
+		var $page,$itemsPerPage;
+
+		// for search
+		var $search;
+
+		protected $db;
+        
+        function __construct($db=null) {
+			if (!empty($db)) 
+	        {
+    	        $this->db = $db;
+        	}
+		}
+		
+		/**
+		 * Inserting into database to add new insurance
+		 * @return result process in json encoded data
+		 */
+		private function doAdd(){
+            if (Auth::validToken($this->db,$this->token,$this->username)){
+                $roles = Auth::getRoleID($this->db,$this->token);
+                if ($roles < 2){			
+        			try {
+		        		$this->db->beginTransaction();
+				        $sql = "INSERT INTO mas_insurance (Insurance,Premium,Min_Premium) 
+        					VALUES (:insurance,:premium,:minpremium);";
+		    			$stmt = $this->db->prepare($sql);
+						$stmt->bindParam(':insurance', $this->insurance, PDO::PARAM_STR);
+						$stmt->bindParam(':premium', $this->premium, PDO::PARAM_STR);
+						$stmt->bindParam(':minpremium', $this->minpremium, PDO::PARAM_STR);
+    					if ($stmt->execute()) {
+	    					$data = [
+		    					'status' => 'success',
+			    				'code' => 'RS101',
+				    			'message' => CustomHandlers::getreSlimMessage('RS101')
+					    	];	
+    					} else {
+	    					$data = [
+		    					'status' => 'error',
+			    				'code' => 'RS201',
+				    			'message' => CustomHandlers::getreSlimMessage('RS201')
+					    	];
+    					}
+	    			    $this->db->commit();
+		    	    } catch (PDOException $e) {
+    			    	$data = [
+	    			    	'status' => 'error',
+		    			    'code' => $e->getCode(),
+    		    			'message' => $e->getMessage()
+	    		    	];
+		    		    $this->db->rollBack();
+    			    }
+                } else {
+                    $data = [
+                        'status' => 'error',
+                        'code' => 'RS404',
+                        'message' => CustomHandlers::getreSlimMessage('RS404')
+                    ];
+                }
+            } else {
+                $data = [
+                    'status' => 'error',
+                    'code' => 'RS401',
+                    'message' => CustomHandlers::getreSlimMessage('RS407')
+                ];
+            }
+			
+			return $data;
+			$this->db = null;
+        }
+        
+
+		/**
+		 * Determine if insurance name is already exist or not
+		 * @return boolean true / false
+		 */
+		private function isInsuranceExist(){
+			$r = false;
+			$sql = "SELECT a.Insurance
+				FROM mas_insurance a 
+				WHERE a.Insurance = :insurance;";
+			$stmt = $this->db->prepare($sql);
+			$stmt->bindParam(':insurance', $this->insurance, PDO::PARAM_STR);
+			if ($stmt->execute()) {	
+            	if ($stmt->rowCount() > 0){
+	                $r = true;
+    	        }          	   	
+			} 		
+			return $r;
+			$this->db = null;
+		}
+
+		/** 
+		 * Add new insurance
+		 * @return result process in json encoded data
+		 */
+		public function add(){
+			if ($this->isInsuranceExist() == false){
+                $data = $this->doAdd();
+            } else {
+                $data = [
+                    'status' => 'error',
+                    'code' => 'RS603',
+                    'message' => CustomHandlers::getreSlimMessage('RS603')
+                ];
+            }
+			
+			return json_encode($data,JSON_PRETTY_PRINT);
+        }
+        
+        /** 
+         * Update insurance
+         *
+         * @return json encoded data
+         */
+		public function update(){
+			if (Auth::validToken($this->db,$this->token,$this->username)){
+                $roles = Auth::getRoleID($this->db,$this->token);
+                if ($roles < 3){
+                    $newinsuranceid = Validation::integerOnly($this->insuranceid);
+		    		try{
+                        $this->db->beginTransaction();
+    
+                        $sql = "UPDATE mas_insurance a SET a.Insurance=:insurance,a.Premium=:premium,a.Min_Premium=:minpremium
+                            WHERE a.InsuranceID = :insuranceid;";
+                        $stmt = $this->db->prepare($sql);
+						$stmt->bindParam(':insurance', $this->insurance, PDO::PARAM_STR);
+						$stmt->bindParam(':premium', $this->premium, PDO::PARAM_STR);
+						$stmt->bindParam(':minpremium', $this->minpremium, PDO::PARAM_STR);
+                        $stmt->bindParam(':insuranceid', $newinsuranceid, PDO::PARAM_STR);
+                        $stmt->execute();
+                    
+                        $this->db->commit();
+                        
+                        $data = [
+                            'status' => 'success',
+                            'code' => 'RS103',
+                            'message' => CustomHandlers::getreSlimMessage('RS103')
+                        ];
+                    } catch (PDOException $e){
+                        $data = [
+                            'status' => 'error',
+                            'code' => $e->getCode(),
+                            'message' => $e->getMessage()
+                        ];
+                        $this->db->rollBack();
+					}
+                } else {
+                    $data = [
+                        'status' => 'error',
+                        'code' => 'RS404',
+                        'message' => CustomHandlers::getreSlimMessage('RS404')
+                    ];
+				}
+			} else {
+				$data = [
+	    			'status' => 'error',
+				    'code' => 'RS401',
+					'message' => CustomHandlers::getreSlimMessage('RS401')
+    			];
+			}
+			
+		    return json_encode($data);
+    		$this->db = null;
+		}
+
+		/** 
+         * Delete insurance
+         *
+         * @return json encoded data
+         */
+		public function delete(){
+			if (Auth::validToken($this->db,$this->token,$this->username)){
+                $roles = Auth::getRoleID($this->db,$this->token);
+                if ($roles == '1'){
+                    $newinsuranceid = Validation::integerOnly($this->insuranceid);
+    				try{
+                        $this->db->beginTransaction();
+    
+                        $sql = "DELETE FROM mas_insurance WHERE InsuranceID = :insuranceid;";
+                        $stmt = $this->db->prepare($sql);
+                        $stmt->bindParam(':insuranceid', $newinsuranceid, PDO::PARAM_STR);
+                        
+						if ($stmt->execute()) {
+    						$data = [
+	    						'status' => 'success',
+		    					'code' => 'RS104',
+			    				'message' => CustomHandlers::getreSlimMessage('RS104')
+				    		];	
+					    } else {
+    						$data = [
+	    						'status' => 'error',
+		    					'code' => 'RS204',
+			    				'message' => CustomHandlers::getreSlimMessage('RS204')
+				    		];
+						}
+						$this->db->commit();
+                    } catch (PDOException $e){
+                        $data = [
+                            'status' => 'error',
+                            'code' => $e->getCode(),
+                            'message' => $e->getMessage()
+                        ];
+                        $this->db->rollBack();
+                    }
+                } else {
+                    $data = [
+                        'status' => 'error',
+                        'code' => 'RS404',
+                        'message' => CustomHandlers::getreSlimMessage('RS404')
+                    ];
+                }
+			} else {
+				$data = [
+	    			'status' => 'error',
+				    'code' => 'RS401',
+					'message' => CustomHandlers::getreSlimMessage('RS401')
+    			];
+			}
+		    return json_encode($data);
+    		$this->db = null;
+		}
+
+		/** 
+		 * Search all data insurance paginated
+		 * @return result process in json encoded data
+		 */
+		public function searchInsuranceAsPagination() {
+			if (Auth::validToken($this->db,$this->token)){
+				$search = "%$this->search%";
+				//count total row
+				$sqlcountrow = "SELECT count(a.InsuranceID) as TotalRow 
+					from mas_insurance a
+					where a.InsuranceID like :search
+                    or a.Insurance like :search
+                    order by a.Insurance asc;";
+				$stmt = $this->db->prepare($sqlcountrow);		
+				$stmt->bindParam(':search', $search, PDO::PARAM_STR);
+				
+				if ($stmt->execute()) {	
+    	    		if ($stmt->rowCount() > 0){
+						$single = $stmt->fetch();
+						
+						// Paginate won't work if page and items per page is negative.
+						// So make sure that page and items per page is always return minimum zero number.
+						$newpage = Validation::integerOnly($this->page);
+						$newitemsperpage = Validation::integerOnly($this->itemsPerPage);
+						$limits = (((($newpage-1)*$newitemsperpage) <= 0)?0:(($newpage-1)*$newitemsperpage));
+						$offsets = (($newitemsperpage <= 0)?0:$newitemsperpage);
+
+						// Query Data
+						$sql = "SELECT a.InsuranceID,a.Insurance,a.Premium,a.Min_Premium
+							from mas_insurance a
+							where a.InsuranceID like :search
+                            or a.Insurance like :search
+                            order by a.Insurance asc LIMIT :limpage , :offpage;";
+						$stmt2 = $this->db->prepare($sql);
+						$stmt2->bindParam(':search', $search, PDO::PARAM_STR);
+						$stmt2->bindValue(':limpage', (INT) $limits, PDO::PARAM_INT);
+						$stmt2->bindValue(':offpage', (INT) $offsets, PDO::PARAM_INT);
+						
+						if ($stmt2->execute()){
+							$pagination = new \classes\Pagination();
+							$pagination->totalRow = $single['TotalRow'];
+							$pagination->page = $this->page;
+							$pagination->itemsPerPage = $this->itemsPerPage;
+							$pagination->fetchAllAssoc = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+							$data = $pagination->toDataArray();
+						} else {
+							$data = [
+        	    	    		'status' => 'error',
+		        		    	'code' => 'RS202',
+	    			    	    'message' => CustomHandlers::getreSlimMessage('RS202')
+							];	
+						}			
+				    } else {
+    	    			$data = [
+        	    			'status' => 'error',
+		    	    		'code' => 'RS601',
+        			    	'message' => CustomHandlers::getreSlimMessage('RS601')
+						];
+		    	    }          	   	
+				} else {
+					$data = [
+    	    			'status' => 'error',
+						'code' => 'RS202',
+	        		    'message' => CustomHandlers::getreSlimMessage('RS202')
+					];
+				}
+				
+			} else {
+				$data = [
+	    			'status' => 'error',
+					'code' => 'RS401',
+        	    	'message' => CustomHandlers::getreSlimMessage('RS401')
+				];
+			}		
+        
+			return json_encode($data);
+	        $this->db= null;
+		}
+
+		/** 
+		 * Search all data insurance paginated public
+		 * @return result process in json encoded data
+		 */
+		public function searchInsuranceAsPaginationPublic() {
+			$search = "%$this->search%";
+			//count total row
+			$sqlcountrow = "SELECT count(a.InsuranceID) as TotalRow 
+				from mas_insurance a
+				where a.InsuranceID like :search
+                or a.Insurance like :search
+                order by a.Insurance asc;";
+			$stmt = $this->db->prepare($sqlcountrow);		
+			$stmt->bindParam(':search', $search, PDO::PARAM_STR);
+				
+			if ($stmt->execute()) {	
+    			if ($stmt->rowCount() > 0){
+					$single = $stmt->fetch();
+						
+					// Paginate won't work if page and items per page is negative.
+					// So make sure that page and items per page is always return minimum zero number.
+					$newpage = Validation::integerOnly($this->page);
+					$newitemsperpage = Validation::integerOnly($this->itemsPerPage);
+					$limits = (((($newpage-1)*$newitemsperpage) <= 0)?0:(($newpage-1)*$newitemsperpage));
+					$offsets = (($newitemsperpage <= 0)?0:$newitemsperpage);
+
+					// Query Data
+					$sql = "SELECT a.InsuranceID,a.Insurance,a.Premium,a.Min_Premium 
+						from mas_insurance a
+						where a.InsuranceID like :search
+                        or a.Insurance like :search
+                        order by a.Insurance asc LIMIT :limpage , :offpage;";
+					$stmt2 = $this->db->prepare($sql);
+					$stmt2->bindParam(':search', $search, PDO::PARAM_STR);
+					$stmt2->bindValue(':limpage', (INT) $limits, PDO::PARAM_INT);
+					$stmt2->bindValue(':offpage', (INT) $offsets, PDO::PARAM_INT);
+						
+					if ($stmt2->execute()){
+						$pagination = new \classes\Pagination();
+						$pagination->totalRow = $single['TotalRow'];
+						$pagination->page = $this->page;
+						$pagination->itemsPerPage = $this->itemsPerPage;
+						$pagination->fetchAllAssoc = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+						$data = $pagination->toDataArray();
+					} else {
+						$data = [
+    	    	    		'status' => 'error',
+			    		    'code' => 'RS202',
+	    			        'message' => CustomHandlers::getreSlimMessage('RS202')
+						];	
+					}			
+				} else {
+    	    		$data = [
+            			'status' => 'error',
+	    	    		'code' => 'RS601',
+    			    	'message' => CustomHandlers::getreSlimMessage('RS601')
+					];
+		    	}          	   	
+			} else {
+				$data = [
+        			'status' => 'error',
+					'code' => 'RS202',
+	        		'message' => CustomHandlers::getreSlimMessage('RS202')
+				];
+			}		
+        
+			return json_encode($data);
+	        $this->db= null;
+		}
+        
+    }
