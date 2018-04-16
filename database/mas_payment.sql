fb5784bcb3a768e4094ed8c9d65a73411be03a77
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-04-16 18:59:11 +0700

    - Add new structure database mas_payment.sql

---
+++ database/mas_payment.sql
@@ -0,0 +1,38 @@
+/*
+Navicat MariaDB Data Transfer
+
+Source Server         : CAP-EXPRESS
+Source Server Version : 100131
+Source Host           : server.cap-express.co.id:3306
+Source Database       : cap_dev
+
+Target Server Type    : MariaDB
+Target Server Version : 100131
+File Encoding         : 65001
+
+Date: 2018-04-16 16:19:14
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for mas_payment
+-- ----------------------------
+DROP TABLE IF EXISTS `mas_payment`;
+CREATE TABLE `mas_payment` (
+  `PaymentID` int(11) NOT NULL AUTO_INCREMENT,
+  `Payment` varchar(50) NOT NULL,
+  PRIMARY KEY (`PaymentID`),
+  KEY `PaymentID` (`PaymentID`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of mas_payment
+-- ----------------------------
+INSERT INTO `mas_payment` VALUES ('1', 'Cash');
+INSERT INTO `mas_payment` VALUES ('2', 'Cheque');
+INSERT INTO `mas_payment` VALUES ('3', 'Cod');
+INSERT INTO `mas_payment` VALUES ('4', 'Credit');
+INSERT INTO `mas_payment` VALUES ('5', 'Transfer Bank');
+INSERT INTO `mas_payment` VALUES ('6', 'Transfer Form');
+SET FOREIGN_KEY_CHECKS=1;
