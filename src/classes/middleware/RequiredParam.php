1b989660c79c0398d75fabcb5bc8b5c01f82ae34
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-04-22 04:41:07 +0700

    - Update RequiredParam middleware

--- src/classes/middleware/RequiredParam.php
+++ src/classes/middleware/RequiredParam.php
@@ -15,50 +15,108 @@ use PDO;
      *
      * @package    Core reSlim
      * @author     M ABD AZIZ ALFIAN <github.com/aalfiann>
-     * @copyright  Copyright (c) 2016 M ABD AZIZ ALFIAN
+     * @copyright  Copyright (c) 2018 M ABD AZIZ ALFIAN
      * @license    https://github.com/aalfiann/reSlim/blob/master/license.md  MIT License
      */
     class RequiredParam
     {
-        private $parameter;
+        private $parameter,$strict,$regex,$message;
 
-        public function __construct($parameter){
+        /**
+         * Constructor
+         * 
+         * @var parameter is the body of the request parameter
+         * @var strict if is set to true, then parameter is can not be empty. Default is false.
+         * @var regex is to validate the value of the parameter. This will working if you set strict to true.
+         */
+        public function __construct($parameter,$strict=false,$regex=''){
             $this->parameter = $parameter;
+            $this->strict = $strict;
+            $this->regex = $regex;
         }
 
         public function __invoke($request, $response, $next){
-            if($this->validate($request,$this->parameter)){
+            if($this->validate($request,$this->parameter,$this->strict,$this->regex)){
                 $response = $next($request, $response);    
                 return $response;
             } else {
                 $body = $response->getBody();
-                $body->write(json_encode([
-                    'status' => 'error',
-                    'code' => 'RS801',
-                    'message' => CustomHandlers::getreSlimMessage('RS801')
-                ]));
+                if (empty($this->message)){
+                    $body->write(json_encode([
+                        'status' => 'error',
+                        'code' => 'RS801',
+                        'message' => CustomHandlers::getreSlimMessage('RS801')
+                    ]));
+                } else {
+                    $body->write(json_encode([
+                        'status' => 'error',
+                        'code' => 'RS801',
+                        'message' => CustomHandlers::getreSlimMessage('RS801'),
+                        'parameter' => $this->message
+                    ]));
+                }
+                
                 return Cors::modify($response,$body,400);
             }
         }
 
-        private function validate($request,$parameter){
+        private function validateRegex($regex,$key,$value){
+            if(!preg_match($regex, $value)){
+                $this->message[$key] = 'the value is not valid!';
+                return false;
+            }
+            return true;
+        }
+
+        private function validate($request,$parameter,$strict=false,$regex=''){
             $parsedBody = $request->getParsedBody();
+            if ($strict == 'strict') $strict = true;
             if (empty($parsedBody)) return true;
             if (is_array($parameter)){
+                $aa = 0;
                 foreach ($parameter as $singleparam){
                     $tt = 0;
                     foreach ($parsedBody as $key => $value) {
                         if ($key==$singleparam){
-                            $tt += 1;
-                        }    
+                            if($strict){
+                                if (!empty($value)){
+                                    if (!empty($regex)){
+                                        if($this->validateRegex($regex,$key,$value)){
+                                            $tt += 1;    
+                                        }
+                                    } else {
+                                        $tt += 1;
+                                    }
+                                } else {
+                                    $this->message[$key] = 'the value is can not be empty!';
+                                }
+                            } else {
+                                $tt += 1;
+                            }
+                        }
                     }
-                    if($tt == 0) return false;
+                    if($tt == 0) $aa += 1;
                 }
+                if($aa > 0) return false;
             } else {
                 $tt=0;
                 foreach ($parsedBody as $key => $value) {
                     if ($key==$parameter){
-                        $tt +=1;
+                        if($strict){
+                            if (!empty($value)){
+                                if (!empty($regex)){
+                                    if($this->validateRegex($regex,$key,$value)){
+                                        $tt += 1;    
+                                    }
+                                } else {
+                                    $tt += 1;
+                                }
+                            } else {
+                                $this->message[$key] = 'the value is can not be empty!';
+                            }
+                        } else {
+                            $tt += 1;
+                        }
                     }
                 }
                 if($tt == 0) return false;
