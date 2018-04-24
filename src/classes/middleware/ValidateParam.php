4db7e3e3576da8397a58a31b2410a2d437a69c39
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-04-24 11:53:57 +0700

    - Update comments

--- src/classes/middleware/ValidateParam.php
+++ src/classes/middleware/ValidateParam.php
@@ -35,6 +35,15 @@ use \classes\JSON as JSON;
             $this->between = $between;
         }
 
+        /**
+         * Validation middleware invokable class
+         * 
+         * @param \Psr\Http\Message\ServerRequestInterface  $request    PSR7 request
+         * @param \Psr\Http\Message\ResponseInterface       $response   PSR7 response
+         * @param callable                                  $next       Next middleware
+         * 
+         * @return \Psr\Http\Message\ResponseInterface
+         */
         public function __invoke($request, $response, $next){
             if($this->validate($request,$this->parameter,$this->between,$this->regex)){
                 $response = $next($request, $response);    
