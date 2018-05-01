a31993642047931d0c1c7658a6d50d176ac07d8f
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-05-02 04:04:57 +0700

    - Add validation on submit form

--- website/app/app/modul-void.php
+++ website/app/app/modul-void.php
@@ -59,12 +59,12 @@ $refpage = (empty($_GET['ref'])?Core::lang('void'):'<a href="'.$_GET['ref'].'"><
                                 <p class="text-muted"><?php echo Core::lang('help_void')?></p>
                                 <hr>
                                 <form class="form-control-line" id="submitdata">
-                                    <div class="form-group">
+                                    <div id="rcodeid" class="form-group">
                                         <label class="form-control-label"><b><?php echo Core::lang('waybill')?></b></label>
                                         <input id="codeid" type="text" class="form-control" maxlength="13" value="<?php echo $codeid?>" required>
                                         <span class="help-block text-muted"><small><i class="ti-info-alt"></i> <?php echo Core::lang('help_void_waybill')?></small></span>
                                     </div>
-                                    <div class="form-group">
+                                    <div id="desc" class="form-group">
                                         <label class="form-control-label"><b><?php echo Core::lang('reason').' '.Core::lang('void')?></b></label>
                                         <textarea id="description" type="text" style="resize: vertical;" rows="5" class="form-control" maxlength="200" required></textarea>
                                         <span class="help-block text-muted"></span>
@@ -101,32 +101,60 @@ $refpage = (empty($_GET['ref'])?Core::lang('void'):'<a href="'.$_GET['ref'].'"><
     <script>
         function sendVoid(){
             $(function(){
-                $.ajax({
-                    url: Crypto.decode("<?php echo base64_encode(Core::getInstance()->api.'/cargo/transaction/data/void')?>"),
-                    data : {
-                        Username: "<?php echo $datalogin['username']?>",
-                        Token: "<?php echo $datalogin['token']?>",
-                        Waybill: $("#codeid").val(),
-                        Description: $("#description").val()
-                    },
-                    dataType: "json",
-                    type: "POST",
-                    success: function(data) {
-                        if (data.status == "success"){
-                            /* clear from */
-                            $("#submitdata")
-                            .find("input,textarea")
-                            .val("")
-                            .end()
-                            console.log("<?php echo Core::lang('submit').' '.Core::lang('void').' '.Core::lang('status_success')?>");
-                            swal("<?php echo Core::lang('void').' '.Core::lang('status_success')?>", data.message,"success");
-                        } else {
-                            console.log("<?php echo Core::lang('submit').' '.Core::lang('void').' '.Core::lang('status_failed')?>");
-                            swal("<?php echo Core::lang('void').' '.Core::lang('status_failed')?>", data.message,"error");
-                        }
-                    },
-                    error: function(x, e) {}
-                });
+                if (validateCheck()) {
+                    $.ajax({
+                        url: Crypto.decode("<?php echo base64_encode(Core::getInstance()->api.'/cargo/transaction/data/void')?>"),
+                        data : {
+                            Username: "<?php echo $datalogin['username']?>",
+                            Token: "<?php echo $datalogin['token']?>",
+                            Waybill: $("#codeid").val(),
+                            Description: $("#description").val()
+                        },
+                        dataType: "json",
+                        type: "POST",
+                        success: function(data) {
+                            if (data.status == "success"){
+                                /* clear from */
+                                $("#submitdata")
+                                .find("input,textarea")
+                                .val("")
+                                .end()
+                                console.log("<?php echo Core::lang('submit').' '.Core::lang('void').' '.Core::lang('status_success')?>");
+                                swal("<?php echo Core::lang('void').' '.Core::lang('status_success')?>", data.message,"success");
+                            } else {
+                                console.log("<?php echo Core::lang('submit').' '.Core::lang('void').' '.Core::lang('status_failed')?>");
+                                swal("<?php echo Core::lang('void').' '.Core::lang('status_failed')?>", data.message,"error");
+                            }
+                        },
+                        error: function(x, e) {}
+                    });
+                    validateClear();
+                }
+            });
+        }
+
+        function validateCheck(){
+            var result = true;
+            if (!$.trim($('#codeid').val()).length){
+                $("#codeid").addClass('form-control-danger');
+                $("#rcodeid").addClass('has-danger');
+                result = false;
+            }
+            if (!$.trim($('#description').val()).length){
+                $("#description").addClass('form-control-danger');
+                $("#desc").addClass('has-danger');
+                result = false;
+            }
+            return result;
+        }
+
+        function validateClear(){
+            $(function(){
+                console.log('Clear form data...');
+                $("#codeid").removeClass('form-control-danger');
+                $("#rcodeid").removeClass('has-danger');
+                $("#description").removeClass('form-control-danger');
+                $("#desc").removeClass('has-danger');
             });
         }
     </script>
