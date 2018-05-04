e5eb7c1d4546111b5f182d798b1b26b0615e686d
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-05-04 19:26:15 +0700

    - Refactor some router to use validate param from middleware

--- src/routers/index.router.php
+++ src/routers/index.router.php
@@ -5,22 +5,22 @@ use \classes\SimpleCache as SimpleCache;
 
     // GET example api to show all data role
     $app->get('/', function (Request $request, Response $response) {
-        $data = [
-		    'status' => 'success',
-			'code' => '200',
-            'welcome' => 'Hello World, here is the CAP Express API for development use.',
-            'message' => 'To use our API make sure You already have a credential to login or an API Key.',
-            'info' => [
-                'webmaster' => 'M ABD AZIZ ALFIAN (aalfiann@gmail.com)',
-                'documentation' => 'The documentation about this API is on our blogs.'
-            ],
-            'generate_time' => date('Y-m-d h:i:s a', time())
-		];
         $body = $response->getBody();
         $response = $this->cache->withEtag($response, $this->etag2hour.'-'.trim($_SERVER['REQUEST_URI'],'/'));
         if (SimpleCache::isCached(3600)){
             $datajson = SimpleCache::load();
         } else {
+            $data = [
+                'status' => 'success',
+                'code' => '200',
+                'welcome' => 'Hello World, here is the CAP Express API for development use.',
+                'message' => 'To use our API make sure You already have a credential to login or an API Key.',
+                'info' => [
+                    'webmaster' => 'M ABD AZIZ ALFIAN (aalfiann@gmail.com)',
+                    'documentation' => 'The documentation about this API is on our blogs.'
+                ],
+                'generate_time' => date('Y-m-d h:i:s a', time())
+            ];
             $blacklistparam = ["&_=","&query=","&search=","token","apikey","api_key","time","timestamp","time_stamp","etag","key","q","s","k","t"];
             $datajson = SimpleCache::save(json_encode($data, JSON_PRETTY_PRINT),null,$blacklistparam);
         }
