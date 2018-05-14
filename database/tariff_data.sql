a2ddb63c4e7feea5415a41728ea8436214f12241
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-05-14 17:27:00 +0700

    - Change structure tariff data and handling

--- database/tariff_data.sql
+++ database/tariff_data.sql
@@ -10,7 +10,7 @@ Target Server Type    : MariaDB
 Target Server Version : 100131
 File Encoding         : 65001
 
-Date: 2018-03-28 16:23:22
+Date: 2018-05-14 13:40:11
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -22,13 +22,15 @@ DROP TABLE IF EXISTS `tariff_data`;
 CREATE TABLE `tariff_data` (
   `BranchID` varchar(10) NOT NULL,
   `Kabupaten` varchar(255) NOT NULL,
+  `ModeID` int(11) NOT NULL,
   `KGP` decimal(10,0) NOT NULL,
   `KGS` decimal(10,0) NOT NULL,
   `Min_Kg` decimal(5,0) NOT NULL,
   `Estimasi` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`BranchID`,`Kabupaten`),
+  PRIMARY KEY (`BranchID`,`Kabupaten`,`ModeID`),
   KEY `BranchID` (`BranchID`),
-  KEY `Kabupaten` (`Kabupaten`)
+  KEY `Kabupaten` (`Kabupaten`),
+  KEY `ModeID` (`ModeID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- ----------------------------
@@ -37,10 +39,12 @@ CREATE TABLE `tariff_data` (
 DROP TABLE IF EXISTS `tariff_handling`;
 CREATE TABLE `tariff_handling` (
   `Kabupaten` varchar(50) NOT NULL,
+  `ModeID` int(11) NOT NULL,
   `KGP` decimal(10,0) NOT NULL,
   `KGS` decimal(10,0) NOT NULL,
   `Min_Kg` decimal(5,0) NOT NULL,
-  PRIMARY KEY (`Kabupaten`),
-  KEY `Kabupaten` (`Kabupaten`)
+  PRIMARY KEY (`Kabupaten`,`ModeID`),
+  KEY `Kabupaten` (`Kabupaten`),
+  KEY `ModeID` (`ModeID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 SET FOREIGN_KEY_CHECKS=1;
