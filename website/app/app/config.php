f9c01047d033009f874a1268557a08c9d5c4f7c7
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-08-03 19:27:00 +0700

    - Update config

--- website/app/app/config.php
+++ website/app/app/config.php
@@ -4,12 +4,12 @@
             $config['keyword'] = 'CAP Express, Logistics, Blog, Transport, Corporate, Cargo, Truck, Jasa Pengiriman, Pengiriman Barang Murah, Ekspedisi'; //Your keyword website
             $config['description'] = 'CAP Express adalah jasa pengiriman barang murah yang handal dan terpercaya.'; //Your description website
             $config['email'] = 'cs@cap-express.co.id'; //Your default email
-            $config['basepath'] = 'http://localhost:1337/cap-dev-repo/website/app/app'; //Your folder website
-            $config['homepath'] = 'http://localhost:1337/cap-dev-repo/website/app/app'; //Your folder frontend website
-            $config['assetspath'] = 'http://localhost:1337/cap-dev-repo/website/app/assets'; //Your folder assets website
-            $config['api'] = 'http://server.cap-express.co.id/api'; //Your folder rest api
-            $config['apikey'] = '2pHslKjfVdHiJAf0cFRfNq5oZXBJRjHOdOQMifccm9GK3kq0xJ5sQygz9'; //Your api key, you can leave this blank and fill this later
-            $config['disqus'] = 'omovv'; //Your disqus username, you can leave this blank and fill this later
+            $config['basepath'] = 'https://cap-express.co.id/app'; //Your folder website
+            $config['homepath'] = 'https://cap-express.co.id/app'; //Your folder frontend website
+            $config['assetspath'] = 'https://cap-express.co.id/assets'; //Your folder assets website
+            $config['api'] = 'https://global.cap-express.co.id/api'; //Your folder rest api
+            $config['apikey'] = 'bjOR28f04qug9anJ6b4YCJcgFDzKNN7uhoDDr2dslpXMEeBzRQtuyT2zrqtkAk'; //Your api key, you can leave this blank and fill this later
+            $config['disqus'] = 'cap-express'; //Your disqus username, you can leave this blank and fill this later
             $config['sharethis'] = '598da1b6ea00a30012ce67a0'; //Your sharethis key, you can leave this blank and fill this later
             $config['facebook'] = ''; //Your facebook page, you can leave this blank and fill this later
             $config['twitter'] = ''; //Your twitter page, you can leave this blank and fill this later
@@ -21,5 +21,5 @@
             $config['yandexwebmaster'] = ''; //Your yandex webmaster, you can leave this blank and fill this later
             $config['seopage'] = ''; //Keyword for dynamic page, you can leave this blank and fill this later
             $config['seosite'] = ''; //Keyword for competitor site, you can leave this blank and fill this later
-            $config['recaptcha_sitekey'] = ''; //This is for get response reCaptcha on html, you can leave this blank and fill this later
-            $config['recaptcha_secretkey'] = ''; //This is for verify request reCaptcha on server side, you can leave this blank and fill this later
+            $config['recaptcha_sitekey'] = '6LcmlmcUAAAAAPe0GLorOqEQi3VMkIonkwITDN5g'; //This is for get response reCaptcha on html, you can leave this blank and fill this later
+            $config['recaptcha_secretkey'] = '6LcmlmcUAAAAAJbxVKr8G8yoPOK_IK-p5pD8RA64'; //This is for verify request reCaptcha on server side, you can leave this blank and fill this later
