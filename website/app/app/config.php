fca532d40f59180ffbbae8a14e7ef5e788523dfe
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2018-08-14 12:36:50 +0700

    - Update config

--- website/app/app/config.php
+++ website/app/app/config.php
@@ -4,10 +4,11 @@
             $config['keyword'] = 'CAP Express, Logistics, Blog, Transport, Corporate, Cargo, Truck, Jasa Pengiriman, Pengiriman Barang Murah, Ekspedisi'; //Your keyword website
             $config['description'] = 'CAP Express adalah jasa pengiriman barang murah yang handal dan terpercaya.'; //Your description website
             $config['email'] = 'cs@cap-express.co.id'; //Your default email
-            $config['basepath'] = 'https://cap-express.co.id/app'; //Your folder website
-            $config['homepath'] = 'https://cap-express.co.id/app'; //Your folder frontend website
-            $config['assetspath'] = 'https://cap-express.co.id/assets'; //Your folder assets website
-            $config['api'] = 'https://global.cap-express.co.id/api'; //Your folder rest api
+            $config['basepath'] = 'http://localhost:1337/cap-dev-repo/website/app/app'; //Your folder website
+            $config['homepath'] = 'http://localhost:1337/cap-dev-repo/website/app/app'; //Your folder frontend website
+            $config['assetspath'] = 'http://localhost:1337/cap-dev-repo/website/app/assets'; //Your folder assets website
+            $config['fixedpath'] = ''; //Your path of main rest api, this is required only if you are running multiple reSlim rest api
+            $config['api'] = 'http://server.cap-express.co.id/api'; //Your path of rest api
             $config['apikey'] = 'bjOR28f04qug9anJ6b4YCJcgFDzKNN7uhoDDr2dslpXMEeBzRQtuyT2zrqtkAk'; //Your api key, you can leave this blank and fill this later
             $config['disqus'] = 'cap-express'; //Your disqus username, you can leave this blank and fill this later
             $config['sharethis'] = '598da1b6ea00a30012ce67a0'; //Your sharethis key, you can leave this blank and fill this later
